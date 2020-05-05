dcb79b9195e64f3f4710deab890ab17ed2219eb9
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-05-05 17:45:32 +0300

    added dto package created SocialDTO class

---
+++ src/main/java/com/qa/dto/SocialDTO.java
@@ -0,0 +1,41 @@
+package com.qa.dto;
+
+import com.qa.domain.Post;
+
+public class SocialDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public SocialDTO(){}
+
+    public SocialDTO(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
