75af1687f0f5d44a234b063d60eebe7e3e8106ef
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-05-14 17:24:48 +0300

    made changes to connect in gcp sql

--- src/main/java/com/qa/domain/Post.java
+++ src/main/java/com/qa/domain/Post.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long pid;
     private String pictureLink;
--- src/main/java/com/qa/domain/Social.java
+++ src/main/java/com/qa/domain/Social.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Social {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
